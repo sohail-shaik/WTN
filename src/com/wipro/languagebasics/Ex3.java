8a4fbace52c40d611ae3bac9e5beb09907d7dab0
SOHAIL <SOHAIL@DESKTOP-M8SA5QN>
2020-06-11 12:43:38 +0530

    Language Basics Example-3

--- src/com/wipro/languagebasics/Ex3.java
+++ src/com/wipro/languagebasics/Ex3.java
@@ -7,10 +7,10 @@ class Ex3 {
 	public static void main(String[] args)
 	{
 		Scanner sc= new Scanner(System.in);
-		int x,y;
-		x=Integer.parseInt(args[0]);
-		y=Integer.parseInt(args[1]);
-		System.out.println(x+y);
+		int x=Integer.parseInt(args[0]);
+		int y=Integer.parseInt(args[1]);
+		int s=x+y;
+		System.out.println(s);
 	}
 
 }
