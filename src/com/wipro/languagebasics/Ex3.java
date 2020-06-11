dfc038018a3c645af49c7ceac3045a05ff340127
SOHAIL <SOHAIL@DESKTOP-M8SA5QN>
2020-06-11 12:39:20 +0530

    Language Basics Hand-On Assignment

---
+++ src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,16 @@
+package com.wipro.languagebasics;
+
+import java.io.*;
+import java.util.*;
+
+class Ex3 {
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		int x,y;
+		x=Integer.parseInt(args[0]);
+		y=Integer.parseInt(args[1]);
+		System.out.println(x+y);
+	}
+
+}
