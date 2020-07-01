1cae647bf5e3db18124975210cf106acbef674de
sohail-shaik <60342069+sohail-shaik@users.noreply.github.com>
2020-07-01 09:32:44 +0530

    Add files via upload

---
+++ languagebasics/Ex2.java
@@ -0,0 +1,12 @@
+package com.wipro.languagebasics;
+
+class Ex2 {
+	public static void main(String args[]) {
+		if(args.length>0)
+		{
+		System.out.println("Welcome"+" " + args[0]);
+		}
+	}
+
+}
+ 
